d35001fa7dfa2e9f3f76868b4914793c77e7099e
Eduardo Bonzi <dubonzi@gmail.com>
2018-03-31 17:46:51 -0300

    Setup project to deploy on heroku

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const hbs = require('hbs');
+const fs = require('fs');
 
 const app = express();
 const port = process.env.PORT || '3000'
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/resources'));
 
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} - ${req.path}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) console.log('Erro ao salvar o server.log')
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
 hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
 hbs.registerHelper('screamIt', text => text.toUpperCase());
 
