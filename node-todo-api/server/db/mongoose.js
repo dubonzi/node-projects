bd91f6c4afa380e5ae77bcdb77d254a3b5735ef4
Eduardo Bonzi <dubonzi@gmail.com>
2018-04-07 14:02:02 -0300

    Setup heroku deploy

--- node-todo-api/server/db/mongoose.js
+++ node-todo-api/server/db/mongoose.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE_URL).then((mg) => {
+mongoose.connect(process.env.MONGODB_URI).then((mg) => {
   console.log('Connected to MongoDB Server');
 }).catch((err) => {
   console.log('Could not connect to MongoDB Server', err);
