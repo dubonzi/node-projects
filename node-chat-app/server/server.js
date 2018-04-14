5ac1c931d29c112bb851bb75d0a96a70d2dc8d77
Eduardo Bonzi <dubonzi@gmail.com>
2018-04-14 16:03:50 -0300

    Added connect and disconnect events

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -1,13 +1,26 @@
 const path = require('path');
+const http = require('http');
 const publicPath = path.join(__dirname, '../public');
+const socketIO = require('socket.io');
 
 const express = require('express');
 
 const port = process.env.PORT || 3000;
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New client connected.');
+  
+  socket.on('disconnect', (socket) => {
+    console.log('Client disconnected.');
+  });
+});
+
+
+server.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 })
